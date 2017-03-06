b0cd59d31144929a25e5477337ed6104e1996f47
莫世友 <momo1a@qq.com>
2017-03-06 17:57:40 +0800

    范德萨范德萨

--- tutorial/spiders/yesky_spider.py
+++ tutorial/spiders/yesky_spider.py
@@ -11,11 +11,20 @@ class YeskySpider(CrawlSpider):
         urls = response.xpath('//div[@class="lb_box"]/dl/dt/a/@href').extract()
         self.log(urls)
         for url in urls:
-            file_obj =  open('yesky_detail_urls.txt','a')
-            file_obj.write(url+'\n')
-            file_obj.close()
+            #file_obj =  open('yesky_detail_urls.txt','a')
+            #file_obj.write(url+'\n')
+            #file_obj.close()
+            yield Request(url,callback=self.detail_parse)
+            
         next_url = response.xpath('//div[@class="flym"]/font/a[text()="下一页"]/@href').extract()
         if next_url:
                 self.log('http://pic.yesky.com' + next_url[0])
                 yield Request('http://pic.yesky.com' + next_url[0], callback=self.parse)
-        return    
+    
+    def detail_parse(self,response):
+        
+        detail_urls =  response.xpath('//div[@class="overview"]/ul/li/a/@href').extract()[1:]
+        for url in detail_urls:
+           file_obj =  open('yesky.txt','a')
+           file_obj.write(url+'\n')
+           file_obj.close() 
