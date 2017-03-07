834af832ebb6cd88f2818dd43165f73d948198d0
莫世友 <momo1a@qq.com>
2017-03-07 17:57:54 +0800

    犯得上发射点

--- tutorial/spiders/yesky_spider.py
+++ tutorial/spiders/yesky_spider.py
@@ -1,5 +1,6 @@
 from scrapy.spiders import CrawlSpider
 from scrapy.http import Request
+import json
 # 天极蜘蛛
 class YeskySpider(CrawlSpider):
     name = 'yesky'
@@ -22,9 +23,28 @@ class YeskySpider(CrawlSpider):
                 yield Request('http://pic.yesky.com' + next_url[0], callback=self.parse)
     
     def detail_parse(self,response):
-        
-        detail_urls =  response.xpath('//div[@class="overview"]/ul/li/a/@href').extract()[1:]
+        #img_src = response.xpath('//div[@class="l_effect_img_mid"]/a/img/@src').extract()[0]
+                     
+        detail_urls =  response.xpath('//div[@class="overview"]/ul/li/a/@href').extract()
+        i = 0
         for url in detail_urls:
-           file_obj =  open('yesky.txt','a')
-           file_obj.write(url+'\n')
-           file_obj.close() 
+            yield Request(url,callback=self.detail_sub_parse,meta={'index':i})
+            i = i+1
+            
+              
+    def detail_sub_parse(self,response):
+               
+        index = response.meta.get('index')
+        imgs = []
+        img_src = response.xpath('//div[@class="l_effect_img_mid"]/a/img/@src').extract()[0]
+        if index != 0:
+            imgs.append(img_src)
+        file_obj =  open('index.txt','a')
+        file_obj.write(json.dumps(imgs)+'\n')
+        file_obj.close()
+         
+            
+            
+        
+               
+               
